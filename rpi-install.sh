a90358ceedc5d9b3da5b530601dc15b0d7b27339
jacktooandroid <9480172+jacktooandroid@users.noreply.github.com>
2018-01-29 16:38:11 +0800

    Add files via upload

--- rpi-install.sh
+++ rpi-install.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
+#License
 clear
+echo 'MIT License'
+echo ''
+echo 'Copyright (c) 2018 jacktooandroid'
+echo ''
+echo 'Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:'
+echo ''
+echo 'The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.'
+echo ''
+echo 'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'
+echo ''
+echo 'Installation will continue in 3 seconds...'
+sleep 3
 
 #Prerequisites
+clear
 echo Prerequisites: Checking if you are running as root...
 idinfo=$(id -u)
 if [[ idinfo -eq 0 ]]
