c6a3b11965a0fcdb9766504e542978f3e7216e46
jacktooandroid <9480172+jacktooandroid@users.noreply.github.com>
2020-02-13 23:47:58 +0000

    February 2020 Updates

--- my-install.sh
+++ my-install.sh
@@ -145,6 +145,9 @@ fi
 echo unifi.https.sslEnabledProtocols=TLSv1.2 | sudo tee -a /usr/lib/unifi/data/system.properties
 echo unifi.https.ciphers=TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_DHE_RSA_WITH_AES_256_GCM_SHA384,TLS_DHE_RSA_WITH_AES_128_GCM_SHA256 | sudo tee -a /usr/lib/unifi/data/system.properties
 
+#Enabling High Performance Java Garbage Collector
+echo unifi.G1GC.enabled=true | sudo tee -a /usr/lib/unifi/data/system.properties
+
 #Redirect port 443 to 8443
 sudo iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443
 
