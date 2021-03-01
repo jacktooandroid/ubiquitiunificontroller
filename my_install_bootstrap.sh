566ce2974e76680fb869b37d9172f6c61a3adc38
jacktooandroid <9480172+jacktooandroid@users.noreply.github.com>
2021-03-01 09:54:35 +0800

    Add files via upload

--- my_install_bootstrap.sh
+++ my_install_bootstrap.sh
@@ -89,10 +89,10 @@ sudo apt-get install iptables-persistent -y
 sudo service unifi restart
 
 #Installing Speedtest by Ookla CLI
+sudo apt-get install gnupg1 apt-transport-https dirmngr -y
 export INSTALL_KEY=379CE192D401AB61
-export DEB_DISTRO=$(lsb_release -sc)
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
-echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtestbyookla-cli.list
+echo "deb https://ookla.bintray.com/debian generic main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
 sudo apt-get update
 sudo apt-get install speedtest -y
 
